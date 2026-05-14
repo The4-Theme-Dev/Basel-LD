7b70ca646b6871f4c8318c2503e0e48cb1ae7520
hekichine <chienlx1005@gmail.com>
2026-05-14 16:47:33 +0700

    update image

--- src/config/data.ts
+++ src/config/data.ts
@@ -27,6 +27,39 @@ import headerLayout8Image from '../assets/images/header_layout/layout_8.png';
 import headerLayout9Image from '../assets/images/header_layout/layout_9.png';
 
 
+// Shop pages
+import shopDefaultImage from '../assets/images/pages/shop/default.png';
+import shopAlternativeImage from '../assets/images/pages/shop/alternative.png';
+import shopFullwidthImage from '../assets/images/pages/shop/fullwidth.png';
+import shopBannerImage from '../assets/images/pages/shop/banner-category.png';
+import shopSidebarLeftImage from '../assets/images/pages/shop/sidebar-left.png';
+import shopSidebarRightImage from '../assets/images/pages/shop/sidebar-right.png';
+import shopStandardButtonImage from '../assets/images/pages/shop/standard-button.png';
+import shopSubCollectionImage from '../assets/images/pages/shop/sub-collection.png';
+import shop3column from '../assets/images/pages/shop/3-columns.png';
+
+// Product pages
+import prBackStock from '../assets/images/pages/product/backstock.png';
+import prDefaultImage from '../assets/images/pages/product/default.png';
+import prLinkedImage from '../assets/images/pages/product/linked.png';
+import prSidebarLeftImage from '../assets/images/pages/product/sidebar-left.png';
+import prSidebarRightImage from '../assets/images/pages/product/sidebar-right.png';
+import prSwatchColorImage from '../assets/images/pages/product/swatch-color.png';
+import prSwatchImageImage from '../assets/images/pages/product/swatch-image.png';
+import prSwatchRadioImage from '../assets/images/pages/product/swatch-radio.png';
+import prSwatchRectangleImage from '../assets/images/pages/product/swatch-rectangle.png';
+import prSwatchSimpleImage from '../assets/images/pages/product/swatch-simple.png';
+
+// other pages
+import pageAbout from '../assets/images/pages/pages/about-me.png';
+import pageCart from '../assets/images/pages/pages/cart.png';
+import pageContactUs from '../assets/images/pages/pages/contact-us.png';
+import pageContactUs2 from '../assets/images/pages/pages/contact-us2.png';
+import pageFaqs from '../assets/images/pages/pages/faqs.png';
+import pageMaintenance from '../assets/images/pages/pages/maintenance.png';
+import pageOurGallery from '../assets/images/pages/pages/ourgallery.png';
+import pageOurShop from '../assets/images/pages/pages/ourshop.png';
+
 export const demoItems = [
   {
     title: "Home new fashion",
@@ -142,4 +175,150 @@ export const demoHeaderItems = [
     title: "Header layout 9",
     image: headerLayout9Image,
   }
+]
+
+
+// Shop pages 
+export const shopPages = [
+  {
+    title: "Shop default",
+    url: "https://basel-v4.myshopify.com/collections/all?view=default&preview_theme_id=",
+    image: shopDefaultImage,
+  },
+  {
+    title: "Shop alternative",
+    url: "https://basel-v4.myshopify.com/collections/all?view=alternative&preview_theme_id=",
+    image: shopAlternativeImage,
+  },
+  {
+    title: "Shop fullwidth",
+    url: "https://basel-v4.myshopify.com/collections/all?view=full-width&preview_theme_id=",
+    image: shopFullwidthImage,
+  },
+  {
+    title: "Sidebar left",
+    url: "https://basel-v4.myshopify.com/collections/all?view=sidebar-left&preview_theme_id=",
+    image: shopSidebarLeftImage,
+  },
+  {
+    title: "Sidebar right",
+    url: "https://basel-v4.myshopify.com/collections/all?view=sidebar-right&preview_theme_id=",
+    image: shopSidebarRightImage,
+  },
+  {
+    title: "Shop 3 columns",
+    url: "https://basel-v4.myshopify.com/collections/all?view=3-columns&preview_theme_id=",
+    image: shop3column,
+  },
+  {
+    title: "Banner category",
+    url: "https://basel-v4.myshopify.com/collections/all?view=banner-category&preview_theme_id=",
+    image: shopBannerImage,
+  },
+  {
+    title: "Standard button",
+    url: "https://basel-v4.myshopify.com/collections/all?view=standard-button&preview_theme_id=",
+    image: shopStandardButtonImage,
+  },
+  {
+    title: "Sub collection",
+    url: "https://basel-v4.myshopify.com/collections/all?view=sub-collection&preview_theme_id=",
+    image: shopSubCollectionImage,
+  },
+]
+
+export const productPages = [
+  {
+    title: "Product default",
+    url: "https://basel-v4.myshopify.com/products/backpack-double-strap-1?preview_theme_id=",
+    image: prDefaultImage,
+  },
+  {
+    title: "Product linked",
+    url: "https://basel-v4.myshopify.com/products/combat-hoodie?preview_theme_id=",
+    image: prLinkedImage,
+  },
+  {
+    title: "Sidebar left",
+    url: "https://basel-v4.myshopify.com/products/backpack-double-strap-1?preview_theme_id=&view=sidebar-left",
+    image: prSidebarLeftImage,
+  },
+  {
+    title: "Sidebar right",
+    url: "https://basel-v4.myshopify.com/products/backpack-double-strap-1?preview_theme_id=&view=sidebar-right",
+    image: prSidebarRightImage,
+  },
+  {
+    title: "Swatch color",
+    url: "https://basel-v4.myshopify.com/products/backpack-double-strap-1?preview_theme_id=",
+    image: prSwatchColorImage,
+  },
+  {
+    title: "Swatch image",
+    url: "https://basel-v4.myshopify.com/products/backpack-double-strap-1?preview_theme_id=&view=swatch-image",
+    image: prSwatchImageImage,
+  },
+  {
+    title: "Swatch radio",
+    url: "https://basel-v4.myshopify.com/products/backpack-double-strap-1?preview_theme_id=&view=swatch-radio",
+    image: prSwatchRadioImage,
+  },
+  {
+    title: "Swatch rectangle",
+    url: "https://basel-v4.myshopify.com/products/backpack-double-strap-1?preview_theme_id=&view=swatch-rectangle",
+    image: prSwatchRectangleImage,
+  },
+  {
+    title: "Swatch simple",
+    url: "https://basel-v4.myshopify.com/products/backpack-double-strap-1?preview_theme_id=&view=swatch-simple",
+    image: prSwatchSimpleImage,
+  },
+  {
+    title: "Back stock",
+    url: "https://basel-v4.myshopify.com/products/jhecked-flannel-shirt?preview_theme_id=",
+    image: prBackStock,
+  },
+]
+
+export const otherPages = [
+  {
+    title: "About me",
+    url: "https://basel-v4.myshopify.com/pages/about-me?preview_theme_id=",
+    image: pageAbout,
+  },
+  {
+    title: "Cart",
+    url: "https://basel-v4.myshopify.com/cart?preview_theme_id=",
+    image: pageCart,
+  },
+  {
+    title: "Contact us",
+    url: "https://basel-v4.myshopify.com/pages/contact-us?preview_theme_id=",
+    image: pageContactUs,
+  },
+  {
+    title: "Contact us 2",
+    url: "https://basel-v4.myshopify.com/pages/contact-us-2?preview_theme_id=",
+    image: pageContactUs2,
+  },
+  {
+    title: "FAQs",
+    url: "https://basel-v4.myshopify.com/pages/faqs?preview_theme_id=",
+    image: pageFaqs,
+  },
+  {
+    title: "Maintenance",
+    url: "https://basel-v4.myshopify.com/password?preview_theme_id=",
+    image: pageMaintenance,
+  },
+  {
+    title: "Our gallery",
+    url: "https://basel-v4.myshopify.com/pages/our-gallery?preview_theme_id=",
+    image: pageOurGallery,
+  },
+  {
+    title: "Our shop",
+    url: "https://basel-v4.myshopify.com/pages/our-shop?preview_theme_id=",
+    image: pageOurShop,
+  },
 ]
